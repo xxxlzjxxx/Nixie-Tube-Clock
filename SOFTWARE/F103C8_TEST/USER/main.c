e0aadc6ec138d212e03afdafb61a4a39a4a5ae2f
xxxlzjxxx <1411795365@qq.com>
2019-10-08 15:56:40 +0800

    完成F103C8和L151CB的软件设计

---
+++ SOFTWARE/F103C8_TEST/USER/main.c
@@ -0,0 +1,53 @@
+#include "led.h"
+#include "delay.h"
+#include "sys.h"
+#include "usart.h"
+#include "timer.h"
+#include "key.h"
+#include "tm1650.h"
+#include "usmart.h"
+#include "ds3231.h"
+
+int main(void){	
+	u16 t = 0;
+    u8 Brightness = 1;
+    
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);// 设置中断优先级分组2
+	delay_init();	    	 //延时函数初始化	  
+    uart_init(115200);	 	//串口初始化为9600
+	LED_Init();		  	 	//初始化与LED连接的硬件接口
+    TIM3_Int_Init(4999,7199);//10Khz的计数频率，计数到5000为500ms  
+    usmart_dev.init(72);	//初始化USMART   
+	KEY_Init();          	//初始化与按键连接的硬件接口
+    TM16_INIT(2);
+    DS3231_Init();
+
+	while(1){ 
+        DS3231_ReadDateTime(1);
+        
+        t=KEY_Scan(1);		//得到键值
+		switch(t)
+		{				 
+			case KEY0_PRES:
+				DateTime.Minute++;
+                DS3231_ModifyDateTime(DateTime.Year,DateTime.Month,DateTime.Day,DateTime.Hour,DateTime.Minute,DateTime.Second);
+				break;
+			case KEY1_PRES:
+                DateTime.Minute--;
+                DS3231_ModifyDateTime(DateTime.Year,DateTime.Month,DateTime.Day,DateTime.Hour,DateTime.Minute,DateTime.Second);
+				break;
+            case WKUP_PRES:
+                Brightness++;
+                if(Brightness == 8){
+                    Brightness = 0;
+                }
+                TM16_Set_Brig(Brightness);
+				break;
+			default:
+				break;	
+		}
+        
+        t = DateTime.Hour * 100 + DateTime.Minute;
+        TM16_Display(t, 3);
+	}		 
+}
---
+++ SOFTWARE/L151CB_TEST_STD/HARDWARE/TM1650/tm1650.h
@@ -0,0 +1,25 @@
+#ifndef __TM1650_H__
+#define __TM1650_H__
+
+#include "sys.h"
+
+//设置接口的 pin
+#define TM16_RCC        RCC_AHBPeriph_GPIOB			//RCC
+#define TM16_PORT       GPIOB					//PORT
+#define TM16_SDA        GPIO_Pin_7			//SDA
+#define TM16_SCL        GPIO_Pin_6			//SCL
+#define TM16_ReadSDA() 	GPIO_ReadInputDataBit(TM16_PORT,TM16_SDA)		//读取SDA
+
+#define TM16_SDA_H GPIO_SetBits(TM16_PORT,TM16_SDA)
+#define TM16_SDA_L GPIO_ResetBits(TM16_PORT,TM16_SDA)
+#define TM16_SCL_H GPIO_SetBits(TM16_PORT,TM16_SCL)
+#define TM16_SCL_L GPIO_ResetBits(TM16_PORT,TM16_SCL)
+
+#define TM16_delay(x){dtmelay(x);}		//设置延时函数,方便移植，可修改为自己的延时函数
+void TM16_INIT(unsigned char brig);		//初始化TM1650,  参数为亮度 0-7
+void TM16_Set_Brig(unsigned char BRIG);
+void TM16_Display(unsigned int data,unsigned char di);//显示函数，data为显示的数据，0-99，di设置是否显示小数点，0为都不显示小数点，1为第一位显示小数点，2为第二位显示小数点，3为第一位和第二位都显示小数点
+
+
+#endif
+
